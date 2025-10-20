ba27733ffe968253e375134a7d88f574f5ad1fd3
Valeria Karpachova <xrd132006lera@gmail.com>
2025-10-20 23:27:46 +0300

    Update main.py

--- PythonProject4/main.py
+++ PythonProject4/main.py
@@ -76,7 +76,7 @@ while True:
                 cap.release()
                 cv2.destroyAllWindows()
                 print("Маркер завершення виявлено. Програма завершена.")
-                exit()  # завершение программы
+                exit()  
 
     cv2.imshow('input', frame)
 
